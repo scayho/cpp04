c31da397589b754cf01a57f391639e71c78c5987
scayho <scayhosama@gmail.com>
2023-02-13 21:54:56 +0100

    commented the problem of the base pointer on the derived object

--- popopopo/cpp04/ex00/main.cpp
+++ popopopo/cpp04/ex00/main.cpp
@@ -23,6 +23,9 @@ int main(void)
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+    std::cout << sizeof(Animal) << std::endl;
+    std::cout << sizeof(Cat) << std::endl;
+    std::cout << sizeof(Dog) << std::endl;
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     i->makeSound(); //will output the cat sound!
